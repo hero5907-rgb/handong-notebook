431256a0364e3de0965b020be35abdf37889ec30
hero5907-rgb <hero5907@gmail.com>
2026-02-25 14:59:21 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 // config.js (클럽별로 이 파일만 수정하면 됨)
 window.APP_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzpi93PZlp1PZWFuimbw91d_8Sc-jVXyjBX94oqNF-Yt2NnIkN-6R4f5ke__wIaV4ml7Q/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyG_A67XjGn8IRfwESexybrzrD4DSeSUzHjyeKP6LLfNvexggcPT5rE99hrs4xblxNs/exec",
 
-  clubName: "북포항라이온스클럽",
-  district: "국제라이온스협회 356-E지구",
+  clubName: "한동회",
+  district: "한동대학교 최고경영자과정",
 
   address: "경상북도 포항시 북구 흥해읍 달전로5번길 52-29 제2라이온스회관",
 
   phone: "054-262-5222",
 
-  copyright: "Developed by 북포항라이온스클럽 황문기L",
+  copyright: "Developed by 한동회 55기 황문기",
 
   // (선택) 기본 이미지 (설정시트에 로고/배경이 있으면 그게 우선)
   logoUrl: "./logo.png",
