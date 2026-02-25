cc02c8b912fc542c65841823fcf57516b6c5a733
hero5907-rgb <hero5907@gmail.com>
2026-02-25 16:49:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// 북포항라이온스클럽 수첩 PWA Service Worker
+// 한동회 수첩 PWA Service Worker
 // ✅ 캐시 갱신이 필요할 때는 CACHE_NAME만 올리면 됩니다 (v55 -> v56 ...)
 
-const CACHE_NAME = "bplions-v10.5";
+const CACHE_NAME = "handong-v1.1";
 
 const ASSETS = [
   "./",
