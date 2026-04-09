63785fe44d96acb39dedb828719e97a01997f187
hero5907-rgb <hero5907@gmail.com>
2026-04-09 17:33:46 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1844,24 +1844,6 @@ document.addEventListener("click", (e) => {
 // 현재만 열기
 popup.classList.add("show");
 
-// 🔥 위치 강제 고정 (수정버전)
-const rect = btn.getBoundingClientRect();
-
-// 🔥 정확한 width 측정 (핵심)
-popup.style.visibility = "hidden";
-popup.style.display = "block";
-popup.style.transform = "none";
-
-const w = popup.offsetWidth;
-
-// 🔥 원상복구
-popup.style.visibility = "";
-
-// 🔥 위치 적용
-popup.style.position = "fixed";
-popup.style.top = (rect.top + rect.height / 2) + "px";
-popup.style.left = (rect.left - w - 6) + "px";
-popup.style.transform = "translateY(-50%)";
 
 });
 
