42024741a26bd5402821981f831b50e21092708b
hero5907-rgb <hero5907@gmail.com>
2026-08-03 13:41:46 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.APP_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbyG_A67XjGn8IRfwESexybrzrD4DSeSUzHjyeKP6LLfNvexggcPT5rE99hrs4xblxNs/exec",
 
-  clubName: "한 동 회",
+  clubName: "CEO동문회",
   district: "한동대학교 최고경영자과정",
 
   address: "37554 경북 포항시 북구 흥해읍 한동로 558 한동대학교",
@@ -17,10 +17,10 @@ window.APP_CONFIG = {
 
   // 🔥 여기 추가
   introText: `
-제11대 한동CEO 총동문회 집행부
+제11대 한동CEO 동문회 집행부
       
 
-한동대학교 최고경영자과정 총동문회는
+한동대학교 최고경영자과정 동문회는
 회원 간의 교류와 협력을 통해
 지속적인 성장과 발전을 도모하는 단체입니다.
 
